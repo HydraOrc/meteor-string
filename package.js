6079f636abbca5f653fd9ca3ce38624109df56e3
HydraOrc <hydra0@bigmir.net>
2017-05-20 11:42:56 +0300

    Update readme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'hydraorc:string',
-  version: '1.0.3',
+  version: '1.0.4',
   summary: 'Small, simple and useful package to work with strings',
   git: 'https://github.com/HydraOrc/meteor-string'
 });
